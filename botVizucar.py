68ca9d5b0cce62dcbcb8cd42a2fe304a456160cf
mohamed-rahmani <mohamed.rahmani@edu.univ-paris13.fr>
2024-11-16 15:26:25 +0100

    [ADDED]:botVizucar.py file with Selenium and ChromeDriver

---
+++ botVizucar.py
@@ -0,0 +1,23 @@
+import json
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+
+def configure_driver():
+    """
+    Configure le driver Chrome avec les options nécessaires.
+    
+    Returns:
+        webdriver.Chrome: Instance du driver Selenium configurée.
+    """
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return driver
